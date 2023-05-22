285bfd9c243fba566f315150b89c926cb329ba0e
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-22 16:00:19 +0200

    aggiunte rotte project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,7 @@ Route::middleware(['auth', 'verified'])
     ->prefix('admin')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); //dashboard
+        Route::resource('projects', ProjectController::class);
     });
 
 
